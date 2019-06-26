4380a01e34ad10be3fc1e226f2ccafdf0f23dd8b
Zian Ke <kezian@umich.edu>
2019-06-26 10:09:20 +0800

    P2 checkall.sh

--- p1/checkall.sh
+++ p1/checkall.sh
@@ -4,9 +4,9 @@ for f in `ls p1_records`; do
     echo p1_records/$f
     #echo p1_records/$f > p1_results/$f.txt
 
-    score=`python3 p1/codestyle.py p1_records/$f --silent`
+    score=`python3 codestyle.py p1_records/$f --silent`
     echo $f,$score | tee -a p1_code_check.csv
 
-    #python3 p1/codestyle.py p1_records/$f >> p1_results/$f.txt
+    #python3 codestyle.py p1_records/$f >> p1_results/$f.txt
     #read -n1 -r -p "Press any key to continue..." key
 done

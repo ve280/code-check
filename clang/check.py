d4c635e5daf5fec120e502b52d069ea404dc5688
tc-imba <liuyh615@sjtu.edu.cn>
2019-07-22 22:48:06 +0800

    code check support multiple files

--- clang/check.py
+++ clang/check.py
@@ -1,18 +1,25 @@
+import os
 import re
 import shlex
 import subprocess
 
-from clang.utils import read_file
+from clang.utils import read_file, split_sources_headers, build_full_paths
 
 
 class FunctionDeclaration:
     function_declares = []
 
-    def __init__(self, line):
+    def __init__(self, line, file=''):
+        self.file = file
         self.error = False
 
         # use regexp to parse the start and end line number
-        function_range = re.findall(r"line:(\d+)", line)
+        function_range = re.findall(r"<(.*?)>", line)
+        if not function_range:
+            self.error = True
+            return
+
+        function_range = re.findall(r"[^c][^o][^l0-9]:(\d+)", function_range[0])
         if len(function_range) < 1:
             self.error = True
             return
@@ -23,6 +30,9 @@ class FunctionDeclaration:
             self.end = self.start
         else:
             self.end = int(function_range[1])
+
+        # simply assume one line definition / declaration
+        self.len = self.end - self.start
 
         # use a trick to split the line
         splitter = shlex.shlex(line, posix=True)
@@ -36,14 +46,14 @@ class FunctionDeclaration:
 
         # use another trick to split the args
         splitter = shlex.shlex(args[-1], posix=True)
-        splitter.whitespace += ',()'
+        splitter.whitespace = ',()'
         splitter.whitespace_split = True
         args = list(splitter)
         if len(args) < 1:
             self.error = True
             return
-        self.ret_type = args[0]
-        self.args_type = args[1:]
+        self.ret_type = args[0].strip()
+        self.args_type = map(lambda x:x.strip(), args[1:])
         self.id = len(FunctionDeclaration.function_declares)
         self.body = []
         FunctionDeclaration.function_declares.append(self)
@@ -66,9 +76,11 @@ class Function:
         self.body_comments = 0
         self.name = func_decl.name
         self.prototype = str(func_decl)
+        self.len = func_decl.len
 
     def add_declaration(self, func_decl):
         self.func_declarations.append(func_decl)
+        self.len += func_decl.len
 
     def analyze_comments(self):
         self.prototype_comments = 0
@@ -111,6 +123,80 @@ class Function:
 
     def __str__(self):
         return self.prototype
+
+
+def parse_functions_new(project_dir, files, silent=False, functions=None):
+    if not functions:
+        functions = dict()
+
+    split_sources_headers(files)
+    sources, headers, _ = split_sources_headers(files)
+    files = sources + headers
+    sources_path = build_full_paths(project_dir, sources)
+    files_path = build_full_paths(project_dir, files)
+    p = subprocess.Popen("clang-check -ast-dump %s --extra-arg='-fno-color-diagnostics' --"
+                         % ' '.join(sources_path), shell=True, stdout=subprocess.PIPE)
+    print(sources_path)
+
+    current_file = None
+
+    if not silent:
+        print('parsing function declarations:')
+
+    func_decl_lines = []
+
+    while p.poll() is None:
+        line = p.stdout.readline().decode('utf-8').strip()
+        result = re.findall(r'<(?!(line|col))(.*?), (line|col):.*?>', line)
+        if result:
+            file_name = result[0][1]
+            flag = False
+            for i, file_path in enumerate(files_path):
+                if file_path in file_name:
+                    # print(files[i], line)
+                    current_file = files[i]
+                    flag = True
+                    break
+            if not flag:
+                current_file = None
+
+        if current_file and 'FunctionDecl' in line and 'line' in line:
+            line = line.strip()
+            func_decl_lines.append((line, current_file))
+
+    file_func_decls = {x: [] for x in files}
+
+    for line, file in func_decl_lines:
+        func_decl = FunctionDeclaration(line, file)
+        if not func_decl.error:
+            func_prototype = str(func_decl)
+            if not silent:
+                print('[%s:%d-%d] %s' % (func_decl.file, func_decl.start, func_decl.end, func_decl))
+            if func_prototype not in functions:
+                functions[func_prototype] = Function(func_decl)
+            else:
+                functions[func_prototype].add_declaration(func_decl)
+            file_func_decls[file].append(func_decl)
+        elif not silent:
+            print('error occurred in %s', line)
+
+    if not silent:
+        print('\nparsing cpp files:')
+
+    for i, file in enumerate(files):
+        file_contents = read_file(files_path[i], silent=silent)
+        func_decls = sorted(file_func_decls[file], key=lambda x: x.start)
+        for j, func_decl in enumerate(func_decls):
+            if func_decl.end <= len(file_contents):
+                # print(j, func_decl)
+                if j > 0:
+                    start = max(func_decls[i - 1].end, func_decl.start - 20)
+                else:
+                    start = max(0, func_decl.start - 20)
+                end = func_decl.end
+                func_decl.set_body([(x, file_contents[x]) for x in range(start, end + 1)])
+
+    return functions
 
 
 def parse_functions(main_cpp_name, main_cpp_path, silent=False):
@@ -163,6 +249,10 @@ def parse_comments(functions, silent=False):
         func.analyze_comments()
         if not silent:
             print(func)
-            print('declarations: %d, prototype comments: %d, body comments: %d'
-                  % (len(func.func_declarations), func.prototype_comments, func.body_comments))
+            print('declarations: %d, body length: %d, prototype comments: %d, body comments: %d'
+                  % (len(func.func_declarations), func.len, func.prototype_comments, func.body_comments))
 
+
+functions = parse_functions_new('/home/liu/SJTU/code-check/solution',
+                                ['world_type.h', 'simulation.cpp', 'p3.cpp', 'simulation.h'])
+parse_comments(functions)
--- clang/utils.py
+++ clang/utils.py
@@ -1,4 +1,6 @@
 import chardet
+import re
+import os
 
 
 def read_file(file_path, silent=False):
@@ -6,5 +8,23 @@ def read_file(file_path, silent=False):
         bytes_str = file.read()
         charset = chardet.detect(bytes_str)['encoding']
         if not silent:
-            print('encoding: %s' % charset)
+            print('%s: encoding: %s' % (file_path, charset))
         return bytes_str.decode(charset).split('\n')
+
+
+def split_sources_headers(files):
+    headers = []
+    sources = []
+    other = []
+    for file in files:
+        if re.findall(r'\.(h|hpp|hh|H|hxx|h\+\+)$', file):
+            headers.append(file)
+        elif re.findall(r'\.(c|cpp|cc|C|cxx|c\+\+)$', file):
+            sources.append(file)
+        else:
+            other.append(file)
+    return sources, headers, other
+
+
+def build_full_paths(project_dir, files):
+    return list(map(lambda x: os.path.join(project_dir, x), files))
